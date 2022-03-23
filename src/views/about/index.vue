b51b0a5bc3c69354b4cde6d8bdaf060985c53cc9
zk <596718356@qq.com>
2022-03-23 21:53:42 +0800

    modify about

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -3,9 +3,17 @@
     <div class="n-layout-page-header">
       <n-card :bordered="false" title="关于Aerfa-Tools">
         Aerfa-Tools 网站信息均来自自于个人收集及阿尔法社区,如有侵权请联系作者删除
-        <div class="mt-5">本项目基于vue3+typescript+vuex+Tailwind CSS</div>
-        <div class="font-semibold mt-10">关于个人</div>
-        <div class="mt-2">前端一枚 </div>
+        <div class="mt-5"></div>
+        <div class="font-semibold mt-10">
+          <a href="https://github.com/owlcity/Aerfa" target="_blank">关于项目</a>
+        </div>
+        <div class="mt-2">本项目基于vue3+typescript+vuex+Tailwind CSS </div>
+        <div class="font-semibold mt-10">
+          <a href="https://github.com/owlcity/Aerfa" target="_blank">项目源码</a>
+        </div>
+        <div class="mt-2">尚未开发完成,后续待完善</div>
+        <div class="font-semibold mt-10">关于本人</div>
+        <div class="mt-2">开发者一枚 </div>
       </n-card>
     </div>
   </div>
